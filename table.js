8e3855b2680211dc28c285fcee69f38c7015ce3b
ccq <962092224@qq.com>
2017-03-30 13:21:15 +0800

    init commit

---
+++ table.js
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-03-16 14:14:05
+ * @version $Id$
+ */
+function b1(){
+  document.getElementById('tbl').style.backgroundColor="red";
+  
+}
+function b2(){
+	var myDate =new date
+	document.getElementById('a2').innerHTML="2017-3-16";
+}
+function b3(){
+	tbl.insertRow(2)
+}
+function b6(){
+	window.open("http://www.taobao.com")
+}
